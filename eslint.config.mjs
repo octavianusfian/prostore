7f366dd0a239d90f426eb69ab337497860936dfb
Octavianus <octavianus.fian@gmail.com>
2025-07-19 12:39:52 +0700

    perbaikan eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["lib/generated/prisma/**"], // ⬅️ abaikan Prisma generate code
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
+import os from "os";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    config.watchOptions = {
+      ignored: [
+        "**/node_modules",
+        "**/.next",
+        path.resolve(os.homedir()), // ini menghindari akses ke direktori user
+      ],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
