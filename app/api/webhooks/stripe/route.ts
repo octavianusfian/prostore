44dc31e49d63f0bbd2916d76aa34f3dea06afe71
Octavianus <octavianus.fian@gmail.com>
2025-09-20 21:26:58 +0700

    perbaikan di route stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,7 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(req: NextRequest) {
-  const event = await Stripe.webhooks.constructEvent(
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const event = await stripe.webhooks.constructEvent(
     await req.text(),
     req.headers.get("stripe-signature") as string,
     process.env.STRIPE_WEBHOOK_SECRET as string
